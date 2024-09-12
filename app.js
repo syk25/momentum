bfafed5677d2bc55f398a3f7eeb526258eba6dd7
syk25 <syk2245@gmail.com>
2024-09-12 18:07:32 +0900

    #3.3 Events

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-/* classname 가져오는 법 - 원소들을 가져옴 */
-// const hellos = document.getElementsByClassName('hello'); // 배열
+const title = document.querySelector('div.hello:first-child h1');
+title.style.color = 'blue'; // 스타일도 조작 가능
 
-/* tag이름으로 태그 가져오는 법 */
-// const hello = document.getElementsByTagName('h1'); // 배열
+/* 보통은 자바스크립트가 이벤트를 듣는다
+이벤트란? 와피ㅏ 끊기, 클릭, 마우스 올리기, 내리기, 자스는 다 들을 수 있다. */
+// 클릭 듣기
 
-/* 추천!  쿼리 셀렉터 = css의 선택자처럼 태그를 가져옴*/
-// const title = document.querySelector('.hello h1'); // hello 클래스 내의 태그 가져옴 - css 선택자 활용
-// const title2 = document.querySelector('div h1');
+function handleTitleClick() {
+    title.style.color = 'red'; // 색깔 설정
+    console.log('title was clicked!');
+}
 
-// 태그가 여러개 존재하더라도 쿼리셀렉터는 첫번째꺼만 가져옴
+title.addEventListener('click', handleTitleClick); // 태그가 이벤트를 듣도록 설정, 이벤트 발생시 함수 실행 + 괄호는 제외
 
-const title = document.querySelector('.hello');
-const titleAll = document.querySelectorAll('.hello');
-
-// console.log(title);
-// console.log(title2);
-console.log(title.innerText);
-console.log(titleAll.innerText);
-
-title.innerText = 'Hello';
+/* 이벤트리스터 만드는 법
+1. 태그 만들기
+2. 태그에 이벤트리스터 정의하기
+3. 함수 정의하기
+4. 이벤트리스너 설정하기 */
