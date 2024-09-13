b6e86c29ba77c6da8a5ee584a4b6ca0a73bf400c
syk25 <syk2245@gmail.com>
2024-09-13 15:23:31 +0900

    #3.6 css in javascript

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const h1 = document.querySelector('div.hello:first-child h1');
-h1.style.color = 'blue'; // 스타일도 조작 가능
 
-/* 보통은 자바스크립트가 이벤트를 듣는다
-이벤트란? 와피ㅏ 끊기, 클릭, 마우스 올리기, 내리기, 자스는 다 들을 수 있다. */
-// 클릭 듣기
-
+// 클릭시 색이 교대로 바꾸는 기능 구현하기
+// Q. 왜 변수에 대입해서 값을 바꿀 수가 없지? 왜 직접적으로 넣어야지?
 function handleTitleClick() {
-    h1.style.color = 'red'; // 색깔 설정
-    console.log('h1 was clicked!');
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === 'blue') {
+        newColor = 'tomato'; // 색깔 설정
+    } else {
+        newColor = 'blue';
+    }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener('click', handleTitleClick); // 태그가 이벤트를 듣도록 설정, 이벤트 발생시 함수 실행 + 괄호는 제외
@@ -17,37 +20,3 @@ h1.addEventListener('click', handleTitleClick); // 태그가 이벤트를 듣도
 2. 태그에 이벤트리스터 정의하기
 3. 함수 정의하기
 4. 이벤트리스너 설정하기 */
-
-function handleH1Mouseenter() {
-    h1.style.color = 'green';
-    console.log('pointer on the mouse.');
-}
-
-function handleH1Mouseleave() {
-    h1.style.color = 'purple';
-    console.log('pointer leaved the mouse.');
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = 'tomato';
-}
-
-function handleWindowCopy() {
-    alert('copier!');
-}
-
-function handleWindowOffline() {
-    alert('SOS! No Wifi!');
-}
-
-function handleWindowOnline() {
-    alert('All Good! Wifi Online!');
-}
-
-h1.addEventListener('mouseenter', handleH1Mouseenter);
-h1.addEventListener('mouseleave', handleH1Mouseleave);
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
