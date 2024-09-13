b67460bc33cefe104569fecbbe1586e42643e9f7
syk25 <syk2245@gmail.com>
2024-09-13 19:15:18 +0900

    #3.7 css in js 2

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+// 목표: 제목을 클릭할 때마다 색을 바꾸는 기능 구현
 const h1 = document.querySelector('div.hello:first-child h1');
 
-// 클릭시 색이 교대로 바꾸는 기능 구현하기
-// Q. 왜 변수에 대입해서 값을 바꿀 수가 없지? 왜 직접적으로 넣어야지?
 function handleTitleClick() {
-    const currentColor = h1.style.color; // getter
-    let newColor; // setter
-    if (currentColor === 'blue') {
-        newColor = 'tomato'; // 색깔 설정
+    /* NOTE: 직접 CSS를 변경하는 법 */
+    // const color = h1.style.color; // getter
+    // let newColor; // setter
+    // if (color === 'blue') {
+    //     newColor = 'tomato';
+    // } else {
+    //     newColor = 'blue';
+    // }
+    // h1.style.color = newColor; // setting color
+    // h1.innerText = 'Changed the Color.';
+    // console.log('Changed the color');
+
+    /* NOTE: HTML태그를 변경하는 법 */
+    // h1.className = 'active'; // 클래스 네임을 변경해버림 -> 결과적으로 css 적용
+    const name = h1.className; // h1.className은 getter 이자 setter
+    const clickedClass = 'active';
+    let newName;
+    if (name === clickedClass) {
+        newName = '';
     } else {
-        newColor = 'blue';
+        newName = clickedClass;
     }
-    h1.style.color = newColor;
+    h1.className = newName;
+    console.log(name);
 }
 
 h1.addEventListener('click', handleTitleClick);
-
