3a554cf57c0d6062e0e9b1a9c7ba0a399bccf257
syk25 <syk2245@gmail.com>
2024-09-16 18:15:58 +0900

    #4.1 form submission

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-/* 해야할 일
-1. 사용자에게 이름 묻기
-2. 정보를 저장하기
-3. 스크린에게 보여주기 */
-/* 모든것은 html에서 시작된다. */
+/* 사용자 이름 검증하기 
+input은 조건을 미리 정할 수 있다
+*/
+/* 브라우저가 새로고침하지 않고도 정보를 저장하기 */
 
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleLoginButtonClick() {
-    console.dir(loginInput.value); // element의 속성을 찾으려고할 때 사용하자. 예를 들어 input에 값을 넣고 새로고침할 때 변화 찾기
-
-    console.log("click");
+    const userName = loginInput.value;
+    console.log(userName);
 }
 
 loginButton.addEventListener("click", handleLoginButtonClick);
