629b2f43b2db50db9110a79acbc60d5aaa6e7a09
syk25 <syk2245@gmail.com>
2024-09-16 19:18:34 +0900

    #4.4 Getting username

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-/* 사용자 이름 검증하기 
-input은 조건을 미리 정할 수 있다
-*/
-/* 브라우저가 새로고침하지 않고도 정보를 저장하기 */
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
-function onLoginSubmit(info) {
-    info.preventDefault();
-    console.log(info);
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username} !`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log(username);
 }
 
 function onMovingToLink(event) {
@@ -18,8 +20,7 @@ function onMovingToLink(event) {
     console.log(link.href);
 }
 
-/* 링크 클릭시 alert으로 중지하고 event 정보 받아오기 */
-
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", onMovingToLink);
-// 이벤트 실행시 헨들러 실행 + 첫번째 인수로 이벤트 발생 시 이벤트에 대한 내용을 객체로 전달
+
+// 로그인 클릭시 form 없애기 - css로 적용하기
