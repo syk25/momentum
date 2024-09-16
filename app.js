5b623fcf14209ec7d4db55b24d46bb2216db7d0e
syk25 <syk2245@gmail.com>
2024-09-16 18:27:54 +0900

    #4.2 Events

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ input은 조건을 미리 정할 수 있다
 */
 /* 브라우저가 새로고침하지 않고도 정보를 저장하기 */
 
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleLoginButtonClick() {
-    const userName = loginInput.value;
-    console.log(userName);
+function onLoginSubmit(info) {
+    info.preventDefault();
+    console.log(info);
 }
 
-loginButton.addEventListener("click", handleLoginButtonClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+// 이벤트 실행시 헨들러 실행 + 첫번째 인수로 이벤트 발생 시 이벤트에 대한 내용을 객체로 전달
