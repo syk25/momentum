82663dd6ca61319a2049555687c9f64256f7ecc8
syk25 <syk2245@gmail.com>
2024-09-16 18:02:57 +0900

    #4.0 로그인 버튼 제작

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-// 목표: 제목을 클릭할 때마다 색을 바꾸는 기능 구현
-const h1 = document.querySelector('div.hello:first-child h1');
+/* 해야할 일
+1. 사용자에게 이름 묻기
+2. 정보를 저장하기
+3. 스크린에게 보여주기 */
+/* 모든것은 html에서 시작된다. */
 
-function handleTitleClick() {
-    /* NOTE: classList 속성, contains, remove 메서드 */
-    // const clickedClass = 'active';
-    // if (h1.classList.contains(clickedClass)) {
-    //     h1.classList.remove(clickedClass); // 변경하기보다는 클래스 제거
-    // } else {
-    //     h1.classList.add(clickedClass); // 변경하기보다는 클래스를 추가
-    // }
-    // console.log(h1.className);
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-    /* NOTE: 위의 방법을 한줄로 표현 */
-    h1.classList.toggle('active');
+function handleLoginButtonClick() {
+    console.dir(loginInput.value); // element의 속성을 찾으려고할 때 사용하자. 예를 들어 input에 값을 넣고 새로고침할 때 변화 찾기
+
+    console.log("click");
 }
 
-h1.addEventListener('click', handleTitleClick);
+loginButton.addEventListener("click", handleLoginButtonClick);
