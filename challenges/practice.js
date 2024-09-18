a5c44281949150044e756bc92c56c0d9350224cc
syk25 <syk2245@gmail.com>
2024-09-18 22:12:40 +0900

    #6.0 [feat] quotes 추가

--- challenges/practice.js
+++ challenges/practice.js
@@ -16,12 +16,14 @@ function saveGuessedNumber() {
 }
 
 function handlePlay(event) {
-    event.preventDefault();
-    const machineNumber = makeNumber();
-    const userNumber = saveGuessedNumber();
+    event.preventDefault(); // 이벤트의 기본동작 방지(ex. 페이지 새로고침)
+    const machineNumber = makeNumber(); // 기계 숫자
+    const userNumber = saveGuessedNumber(); // 내가 고른 숫자
+    
     document.getElementById(
         "result"
     ).innerText = `You chose ${userNumber}, the machine chose ${machineNumber}.`;
+
     console.log(typeof machineNumber, typeof userNumber);
     if (machineNumber === userNumber) {
         winOrLose.innerText = "You Win!";
--- js/clock.js
+++ js/clock.js
@@ -20,7 +20,7 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    console.log(`${hours}:${minutes}:${seconds}`);
+    // console.log(`${hours}:${minutes}:${seconds}`);
 }
 
 // 시계 만듦
