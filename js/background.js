50cfae936fc44f9e248dac0ecc4e570f353887a8
syk25 <syk2245@gmail.com>
2024-09-18 22:32:51 +0900

    #6.1 [feat] random background

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = ["image1.png", "image2.png", "image3.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+console.log(bgImage);
+
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);
+
+console.log(bgImage);
