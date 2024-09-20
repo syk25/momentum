fcdd95684728900c0d8f5a4889fd8a170a4ff214
syk25 <syk2245@gmail.com>
2024-09-20 17:54:22 +0900

    # 7.1 adding todos

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,21 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
-const todoList = document.getElementById("todo");
+const todoList = document.getElementById("todo-list");
 
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    paintTodo(newTodo);
 }
 
+/* NOTE: 새로운 태그를 추가하는 방법 */
+function paintTodo(newTodo) {
+    const li = document.createElement("li"); // 새로운 요소 추가
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    todoList.appendChild(li);
+}
 
 todoForm.addEventListener("submit", handleTodoSubmit);
